96ca319b75ab9b42855257c2f13d6b9ab2c6948b
jude sinjeh <sjrteam1@gmail.com>
2020-03-15 20:36:05 +0100

    15042020

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,7 +2,7 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "/index.html": "0016e9947e316e5d7fd68dfda4bc0b66",
-"/main.dart.js": "95a24064be8a8414cd14f27997a72db8",
+"/main.dart.js": "155a5547f29185651ab8156523203f17",
 "/favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
